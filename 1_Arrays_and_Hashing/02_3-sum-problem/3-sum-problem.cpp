bfc426e495b20979ddab555ece7d5380149646c4
Snehil's Github <69778988+snehil-githubrepository@users.noreply.github.com>
2023-09-13 17:20:41 +0530

    problem 3-sum Completed with Description

--- 1_Arrays_and_Hashing/02_3-sum-problem/3-sum-problem.cpp
+++ 1_Arrays_and_Hashing/02_3-sum-problem/3-sum-problem.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
-        vector<vector<int>> triplet(int n, vector<int> &arr) {
-            vector<vector<int>> ans;
-            sort(arr.begin(), arr.end());
-            for (int i = 0; i < n; i++) {
-                //remove duplicates:
-                if (i != 0 && arr[i] == arr[i - 1]) continue;
+    //TIME COMPLEXITY : O(n log n) + O(n x n) ~ O(n log n) + O(n ^ 2)
+    //SPACE COMPLEXITY : O (number of unique triplets) , i.e. ans list that we are using to store answers
 
-                //moving 2 pointers:
-                int j = i + 1;
-                int k = n - 1;
-                while (j < k) {
-                    int sum = arr[i] + arr[j] + arr[k];
-                    if (sum < 0) {
-                        j++;
-                    }
-                    else if (sum > 0) {
-                        k--;
-                    }
-                    else {
-                        vector<int> temp = {arr[i], arr[j], arr[k]};
-                        ans.push_back(temp);
-                        j++;
-                        k--;
-                        //skip the duplicates:
-                        while (j < k && arr[j] == arr[j - 1]) j++;
-                        while (j < k && arr[k] == arr[k + 1]) k--;
-                    }
+    vector<vector<int>> triplet(int n, vector<int> &arr) {
+        vector<vector<int>> ans;
+        sort(arr.begin(), arr.end()); //N LOG N
+        for(int i = 0;i < n; i++) { // N
+            // if i is not first element , we check with previous value that the new a[i] should not have same value as prev a[i] value
+            // if so we will continue to move forward
+            if (i > 0 && arr[i] == arr[i-1]) continue;
+
+            int j = i+1;
+            int k = n-1;
+            while( j < k) { // near about N 
+                int sum = arr[i] + arr[j] + arr[k];
+                if(sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    //when x = 0 condition
+                    //this is my triplet
+                    vector<int> temp = {arr[i], arr[j], arr[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    //also we check while moving j and k that it should not have equal value to prev arr[j] and arr[k]
+                    //so keep moving until it is equivalent to prev
+                    while(j < k && arr[j] == arr[j-1]) j++;
+                    while(j < k && arr[k] == arr[k+1]) k--;
                 }
             }
-            return ans;
-            }
+        }
+        return ans;      
+    }
 };
